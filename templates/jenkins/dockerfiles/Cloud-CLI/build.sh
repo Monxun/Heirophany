8fbf1e6f33fffb61f5a6713a893c8f559096ff2c
Michael Gibney <michaelgibney1@gmail.com>
2022-05-08 23:38:29 -0700

    updated jenkins

--- templates/jenkins/dockerfiles/Cloud-CLI/build.sh
+++ templates/jenkins/dockerfiles/Cloud-CLI/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export IMAGE_NAME=jenkins-sonarqube-agent
+export IMAGE_NAME=cloud-cli
 export IMAGE_REPO=us-east1-docker.pkg.dev/aline-jenkins-gcp/nightwalkers-images
 export BUILD_ID=0.0.1
 
@@ -11,8 +11,6 @@ export BUILD_ID=0.0.1
 # docker build . -t $IMAGE_NAME:$BUILD_ID
 # docker tag $IMAGE_NAME:$BUILD_ID $IMAGE_REPO/$IMAGE_NAME:$BUILD_ID
 # docker push $IMAGE_REPO/$IMAGE_NAME --all-tags
-
-docker login --username=heirophany --email=michaelgibney1@gmail.com
 
 docker build . -t $IMAGE_NAME:$BUILD_ID
 docker tag $IMAGE_NAME:$BUILD_ID heirophany/$IMAGE_NAME:$BUILD_ID
